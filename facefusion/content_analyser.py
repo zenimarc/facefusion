9dcea9a8bce64c8d30a0ed06323716e1954322c6
zenimarc <marco.verzeni@mail.polimi.it>
2024-02-22 18:44:16 +0100

    rm check for efficiency

--- facefusion/content_analyser.py
+++ facefusion/content_analyser.py
@@ -85,6 +85,7 @@ def analyse_image(image_path: str) -> bool:
 
 @lru_cache(maxsize = None)
 def analyse_video(video_path : str, start_frame : int, end_frame : int) -> bool:
+	return False
 	video_frame_total = count_video_frame_total(video_path)
 	video_fps = detect_video_fps(video_path)
 	frame_range = range(start_frame or 0, end_frame or video_frame_total)
